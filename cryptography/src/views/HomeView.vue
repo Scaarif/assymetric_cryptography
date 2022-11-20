dc66ab34ff42bb1432f3524907e57087b9f92cca
scaarifintl <scaarifintl@gmail.com>
2022-11-20 10:49:27 +0300

    improve look: take one

--- cryptography/src/views/HomeView.vue
+++ cryptography/src/views/HomeView.vue
@@ -4,6 +4,9 @@
 
 <template>
   <div class="container">
+    <div class="header">
+      <h1>Master Encryption</h1>
+    </div>
    <div class="source-message display">
       <!-- input (write the message to encrypt) -->
       <div class="input item">
@@ -26,24 +29,30 @@
    </div>
    <div class="decryption display">
       <!-- display encrypted key (senders public key) -->
-      <div class="key">
-        <p>&lt;- your key, encrypted (:just coz we can do that:) -></p>
-        <span>actual key</span>
+      <div class="key item">
+        <h2>your key, encrypted (:just coz we can do that:)</h2>
+        <span class="line"></span>
+        <span>{{ sendersKeyEncrypted }}</span>
       </div>
       <!-- Display encrypted version (of message)? -->
-      <div class="original-text">
-        <p>There we are! It's all encrypted and safely on transit...</p>
-        <span>actual message</span>
+      <div class="original-text item">
+        <h2>There we are! It's all encrypted and safely on transit...</h2>
+        <span class="line"></span>
+        <p>{{ encryptedMessage }}</p>
       </div>
     </div>
     <div class="received-message display">
       <!-- display decrypted key (senders public key) -->
-      <div class="key">
-        <p>&lt;- senders key, decrypted -></p>
+      <div class="key item">
+        <h2>&lt;- senders key, decrypted -></h2>
+        <span class="line"></span>
+        <span>{{ decryptedKey }}</span>
       </div>
       <!-- Display decrypted version (of message)? -->
-      <div class="original-text">
-        <p>...And, your message, safely delivered & readable:)</p>
+      <div class="original-text item">
+        <h2>...And, your message, safely delivered & readable:)</h2>
+        <span class="line"></span>
+        <p>{{ decryptedMessage }}</p>
       </div>
     </div>
   </div>
@@ -53,8 +62,11 @@ export default {
   data() {
     return {
       sendersMessage: '',
-      actualText: '',
       sendersKey: 'hello there!',
+      sendersKeyEncrypted:'!ereht olleh',
+      encryptedMessage:'on transit, safe and sound...',
+      decryptedKey:'hello there!',
+      decryptedMessage:'well, there you have it!',
     }
   }
 }
@@ -65,6 +77,17 @@ export default {
   flex-direction: column;
   min-height: 100vh;
   width: 100%;
+  padding-right: 1rem;
+}
+.header{
+  margin-bottom: 1rem;
+  margin-top: 0;
+  padding-block: 1rem;
+  border-block: 1px solid var(--color-background-soft);
+  text-align: center;
+  text-transform: capitalize;
+  color:#f2f2f2;
+  letter-spacing: .1rem;
 }
 h2 {
   text-align: center;
@@ -77,13 +100,10 @@ h2 {
   padding: 2rem;
 }
 .item {
-  min-width: 400px;
+  min-width: 380px;
   border: 1px solid var(--color-border);
   border-radius: 30px;
   padding: 2rem;
-}
-.item:last-of-type{
-  /* border-right: none; */
 }
 
 .line:first-of-type {
@@ -94,7 +114,7 @@ h2 {
   display: block;
   border: 1px solid orange;
 }
-.source-message {
+.source-message, .decryption, .received-message {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -122,7 +142,12 @@ h2 {
   color: #f2f2f2;
 }
 
-.display div:nth-child(3) p
+.display div:nth-child(3) p,
+.source-message div:nth-child(2) span:last-child,
+.decryption div:nth-child(2) p,
+.decryption div:nth-child(1) span:last-child,
+.received-message div:nth-child(2) p,
+.received-message div:nth-child(1) span:last-child
 {
   /* border: 1px solid red; */
   margin-top: 1rem;
@@ -131,13 +156,55 @@ h2 {
   border-bottom-right-radius: 15px;
   background: var(--color-background-soft);
 }
-.decryption {
+.decryption div:nth-child(1) span:last-child,
+.received-message div:nth-child(1) span:last-child,
+.decryption div:nth-child(2) p
+{
+  border-bottom-right-radius: 0;
 
 }
+.received-message div:nth-child(1) span:last-child,
+.source-message div:nth-child(2) span:last-child
+{
+  border-bottom-left-radius: 15px;
+}
+.decryption .item,
+.received-message .item
+{
+  min-width: 40%;
+}
+.decryption .line
+{
+  border: 1px solid var(--color-text);
+}
+.decryption .key span:last-child,
+.decryption .original-text p {
+  color: orange;
+}
 
-.received-message {
-  /* border: 1px solid var(--color-border); */
-  /* height: 100px; */
-  /* width: 1000px; */
+.received-message .line
+{
+  border: 1px solid greenyellow;
+
+}
+.decryption .item {
+  border: none;
+}
+.decryption .item:first-of-type {
+  border-left: 1px solid var(--color-background-soft);
+}
+.decryption .item:last-of-type {
+  border-right: 1px solid var(--color-background-soft);
+}
+.received-message .item {
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+.received-message .item:first-of-type {
+  border-bottom-right-radius: 0;
+}
+
+.received-message .item:last-of-type {
+  border-bottom-left-radius: 0;
 }
 </style>
